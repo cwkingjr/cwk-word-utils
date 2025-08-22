ee0f30082455755dcee4c0dcf47968c969934f67
Chuck King <cwkingjr@gmail.com>
2025-08-22 10:50:46 -0500

    added remove stop words functions

---
+++ src/cwk_word_utils/remove_stop_words.py
@@ -0,0 +1,56 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import spacy
+
+# https://www.nltk.org/data.html
+
+
+def remove_stopwords_nltk(text, nltk_corpus_language="english") -> list:
+    """Removes stopwords using nltk stopwords and word_tokenize.
+
+    Requires that `nltk` corpus `stopwords` and `punkt` have been previously
+    downloaded and installed on your system in the location identified by
+    the nltk data link below. Or, you can put the data somewhere else and
+    inform nltk via enviroment variables. See how to set those up in the
+    data link: https://www.nltk.org/data.html.
+
+    You can download the files manually with python via:
+    import nltk
+    nltk.download("stopwords")
+    nltk.download("punkt")
+
+    A quick way to do this, if you have uv installed, is to put that info above,
+    into a get_nltk_data.py file, then run this command:
+    `uv run --isolated --with nltk python3 ./get_nltk_data.py`.
+    """
+
+    # Get English stopwords and tokenize
+    stop_words = set(stopwords.words("english"))
+    tokens = word_tokenize(text.lower())
+
+    # Remove stopwords
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+
+    return filtered_tokens
+
+
+def remove_stopwords_spacy(text) -> list:
+    """Removed stop words using spacy nlp.
+
+    Loads spacy en_core_web_sm, gets tokens from text via spacy nlp, and filters
+    out stop words.
+
+    Expects `en_core_web_sm` to be previously downloaded to your system with:
+    `python3 -m spacy download en_core_web_sm`
+
+    If you have only ever use `uv`, you can accomplish this with these commands:
+
+    - `uv python install`
+    - `uv pip install pip spacy`
+    - `python3 -m spacy download en_core_web_sm`
+    """
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+
+    filtered_words = [token.text for token in doc if not token.is_stop]
+    return filtered_words
---
+++ tests/test_remove_stop_words.py
@@ -0,0 +1,20 @@
+import pytest
+
+from cwk_word_utils.remove_stop_words import (
+    remove_stopwords_nltk,
+    remove_stopwords_spacy,
+)
+
+
+def test_remove_stopwords_nltk():
+    text = "This is a sample sentence showing stopword removal."
+    result = remove_stopwords_nltk(text)
+    expected = ["sample", "sentence", "showing", "stopword", "removal", "."]
+    assert result == expected
+
+
+def test_remove_stopwords_spacy():
+    text = "This is a sample sentence showing stopword removal."
+    result = remove_stopwords_spacy(text)
+    expected = ["sample", "sentence", "showing", "stopword", "removal", "."]
+    assert result == expected
